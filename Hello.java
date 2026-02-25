c0e48f956cf5d6cc8d4fc137e28a4d9bc2fc6831
2025pushkarjaju <2025.pushkar.jaju@ves.ac.in>
2026-02-25 14:53:11 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println("Sum = " + (a + b));
     }
 }
